ef28bbce6cc4ca9bdd43a6ec2834dab69b598922
Aaron Schlesinger <arschles@gmail.com>
2018-09-19 10:43:26 -0700

    adding redis interaction

--- demo3/server.py
+++ demo3/server.py
@@ -5,12 +5,17 @@ import redis
 
 PORT = 8000
 
+r = redis.StrictRedis(host='localhost', port=6379, db=0)
+
+counter = 1
+
 class SimpleHTTPRequestHandler(BaseHTTPRequestHandler):
 
     def do_GET(self):
+        r.Set("thecounter", counter)
         self.send_response(200)
         self.end_headers()
-        self.wfile.write(b'Hello, world!')
+        self.wfile.write(b'Hey VSLive! thecounter = ' + counter)
 
 # Handler = SimpleHTTPServer.SimpleHTTPRequestHandler
 Handler = SimpleHTTPRequestHandler
